9e52c4c3bc3a2c3d8ed3d3b0b080168966914a3a
Joshua Olatunji <joshua+github@etentlabs.com>
2024-08-02 08:55:26 +0100

    Application configuration for talentpool

---
+++ talentpool/apps.py
@@ -0,0 +1,12 @@
+"""
+Application configuration for talentpool
+"""
+from django.apps import AppConfig
+
+
+class TalentpoolConfig(AppConfig):
+    """
+    Talentpool app configuration
+    """
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'talentpool'
