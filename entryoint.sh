de8a3616fc566277b86c08df81129b89795883a9
Joshua Olatunji <joshua+github@etentlabs.com>
2024-08-02 09:41:52 +0100

    the entrypoint file

---
+++ entryoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate --noinput
+
+# Seed database with username bent, password 5478
+python manage.py create_user bent 5478
+# test
+python manage.py test
+
+# Start Gunicorn server
+gunicorn -b :8000 job_board.wsgi
