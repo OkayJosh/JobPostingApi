d6989f5dfbfc1cb2d22f1c130a93dad937cd377b
Joshua Olatunji <joshua+github@etentlabs.com>
2024-08-02 08:53:33 +0100

    job_board URL Configuration

---
+++ job_board/urls.py
@@ -0,0 +1,10 @@
+"""job_board URL Configuration"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include("talentpool.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
